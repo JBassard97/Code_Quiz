81bd4982363aba61a4b46e91ce9493f34717697e
Jbassard <jonathanacciarito@gmail.com>
2023-10-10 15:40:59 -0400

    timer working and ready to start writing quiz

--- script.js
+++ script.js
@@ -4,13 +4,36 @@
 // var questionField = document.getElementsByClassName("questionField");
 // var startButton = document.getElementById("startButton");
 
-let timeLeft = 75;
+var startButton = document.getElementById("startButton");
+startButton.addEventListener("click", codeQuiz);
+var timeLeft = 75;
 document.querySelector(".secondLeft").textContent = timeLeft + " seconds left.";
-document.querySelector(".questionField").textContent = "Coding Quiz Challenge";
-document.querySelector(".answerField").textContent =
+var questionField = document.querySelector(".questionField");
+questionField.textContent = "Coding Quiz Challenge";
+// document.querySelector(".questionField").textContent = "Coding Quiz Challenge";
+var answerField = document.querySelector(".answerField");
+answerField.textContent =
   "Do your best to answer these coding questions before time runs out! Each incorrect answer will penalize you by 10 seconds!";
 
-function codeQuiz() {}
+function codeQuiz() {
+  var timeLeft = 75;
+  var timerInterval = setInterval(function () {
+    timeLeft--;
+    document.querySelector(".secondLeft").textContent =
+      timeLeft + " seconds left.";
+
+    if (timeLeft === 0) {
+      clearInterval(timerInterval);
+      window.alert("Don't be discouraged, try again!");
+    }
+  }, 1000);
+
+  questionField.textContent =
+    "Which of the following are NOT a JavaScript data type?";
+  questionField.setAttribute("style", "font-size:25px");
+  answerField.textContent = "";
+  startButton.setAttribute("style", "display:none");
+}
 
 // function setTime() {
 //   // Sets interval in variable
@@ -32,4 +55,3 @@ function codeQuiz() {}
 // event listener to h3 to open highscore page
 
 // event listener for start quiz button
-document.querySelector("#startButton").addEventListener("click", codeQuiz());
